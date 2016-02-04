67c93fc10c66f08618dfe9be9cf90e43ab6c5a83
thurmand <twintrool@gmail.com>
2016-02-04 12:25:17 -0700

    more updates

---
+++ ATsandbox/triggereventstorage/event.js
@@ -0,0 +1,12 @@
+function load(){
+    console.log("Loading")
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#123"
+    var touch = document.getElementById("field")
+    touch.addEventListener("touchstart", touchHandler, false)
+    console.log("Done Loading")
+}
+
+function touchHandler(event){
+    var coords = document.getElementById("addText")
+    coords.innerHTML = "x: " + event.touches[0].pageX + ", y: " + event.touches[0].pageY
+}
