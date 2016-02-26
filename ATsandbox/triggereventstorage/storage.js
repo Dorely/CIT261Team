7ba62a7bb6f718162ea063d42f1cf0070c7010b1
thurmand <twintrool@gmailcom>
2016-02-25 21:27:28 -0700

    added storage happy and nasty paths

--- ATsandbox/triggereventstorage/storage.js
+++ ATsandbox/triggereventstorage/storage.js
@@ -1,14 +1,66 @@
 //STORAGE
 
 function localStorage(){
-    // happy path to creating storage   
-    //localStorage.setItem("lastname", "Thurman") // for some reason it says it's not a function.
+      
+    //localStorage.setItem("lastname", "Thurman") // for some reason it says it's not a function. setItem
     //localStorage.getItem("lastname") // ditto
     
-    
+    // happy path to creating storage 
     localStorage.lastname = "Thurman"
     
-    console.log(localStorage.lastname)
-    
     //localStorage.removeItem("lastname") // ditto
+    
+    var list = [1,3,7,5,9]
+    // storing an array
+    localStorage.numList = list
+    
+    var associArray = {"array":[
+        {"first":"Andrew", "last":"Thurman"},
+        {"first":"Clarke", "last":"White"},
+        {"first":"Jared", "last":"Thurman"}]}
+    
+    localStorage.obj = JSON.stringify(associArray)
+    
+    var obj1 = {
+        num: 3,
+        arry: [1, "Fish", 2, "Fish"],
+        method: function(){
+            return true
+        }    
+    }
+    
+    localStorage.object = obj1
+    
+    //nasty path works for both local and session
+    //localStorage.88 = "Outatime" Unexpected number COMPILER ERROR
+    //can't store stuff with numbers
+    localStorage.null = "string"
+
+}
+
+function sessionStorage(){
+    
+    sessionStorage.fName = document.getElementById("fName").value
+    
+}
+
+function displayLocal(){
+    //happy path on retriving items
+    console.log(localStorage.numList)
+    
+    console.log(localStorage.obj)
+    
+    console.log(localStorage.object)
+    
+    //nasty path
+    //not stored in Storage
+    console.log(localStorage.null) // comes back a undefined
+    // a number
+    //console.log(localStorage.6) compiler error
+}
+
+function displaySession(){
+    
+     console.log(sessionStorage.fName)
+    
 }
