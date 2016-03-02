1018482defcea480a3bb67101a813d534067364e
thurmand <twintrool@gmail.com>
2016-03-01 18:23:27 -0700

    Added a comment block

---
+++ WhiteBoardProject/load.js
@@ -0,0 +1,10 @@
+/***
+* Author: Andrew Thurman
+*
+* load.js
+*
+* This file helps the user load an image into the canvas so that 
+* the user can draw on the image. The image can either be loaded
+* from a link given by the user or an image from the user's
+* phone. 
+*/
